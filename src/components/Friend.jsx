fea28a6f4038bf38483d508b88dad072f9e891d3
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-03 21:59:56 +0300

    Correct color for even bills

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -8,7 +8,7 @@ function Friend({friend}) {
 
         {friend.balance < 0 && <p className='red'>You owe {friend.name} {Math.abs(friend.balance)}$</p>}
         {friend.balance > 0 && <p className='green'>{friend.name} owes you {Math.abs(friend.balance)}$</p>}
-        {friend.balance === 0 && <p className='red'>You and {friend.name} are even</p>}
+        {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
         <Button>Select</Button>
     </li>
