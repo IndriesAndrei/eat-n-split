4d8b3b14721bb96e0b6a927ba61424010f557582
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-03 21:55:28 +0300

    Add friend to list form functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
+import { useState } from 'react'
 import './App.css'
+import Button from './components/Button'
+import FormAddFriend from './components/FormAddFriend'
+import FormSplitBill from './components/FormSplitBill'
 import FriendsList from './components/FriendsList'
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  }
+]
+
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function handleShowAddFriend() {
+    setShowAddFriend(show => !show)
+  }
+
+  function handleAddFriend(friend) {
+    // create new friends array and add the new friend to it
+    setFriends(friends => [...friends, friend]);
+    // hide the form again after adding a new friend
+    setShowAddFriend(false);
+  }
+
   return (
-    <div className='app'>
-      <div className='sidebar'>
-        <FriendsList />
+    <>
+      <h1>-- Eat And Split --</h1>
+      <hr />
+      <div className='app'>
+        <div className='sidebar'>
+          <FriendsList friends={friends} />
+          {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+          <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add friend'}</Button>
+        </div>
+
+        <FormSplitBill />
       </div>
-    </div>
+    </>
   )
 }
 
--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,27 +1,9 @@
-const initialFriends = [
-    {
-      id: 118836,
-      name: "Clark",
-      image: "https://i.pravatar.css/48?u=118836",
-      balance: -7,
-    },
-    {
-      id: 933372,
-      name: "Sarah",
-      image: "https://i.pravatar.css/48?u=933372",
-      balance: 20,
-    },
-    {
-      id: 499476,
-      name: "Anthony",
-      image: "https://i.pravatar.css/48?u=499476",
-      balance: 0,
-    }
-]
+import Friend from './Friend'
 
-function FriendsList() {
-    const friends = initialFriends;
-    return <ul>Friends</ul>
+function FriendsList({friends}) {
+    return <ul>
+        {friends.map(friend => <Friend friend={friend} key={friend.id} />)}
+      </ul>
 }
 
 export default FriendsList;
